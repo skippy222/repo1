bd8de1a163137abcc86c3bfa908c87975cc95904
skipp <skipp.22@hotmail.com>
2021-01-06 14:18:38 -0700

    updated forloop

--- forloopscript.sh
+++ forloopscript.sh
@@ -1,7 +1,7 @@
   #!/bin/bash
   #failed SSH attempts
 
-  for ((x = 0 ; x < 250 ; x++));; do
+  for ((x = 0 ; x < 250 ; x++)); do
             ssh azadmin@10.2.0.7
             ssh azadmin@10.2.0.8
             ssh azadmin@10.2.0.10
